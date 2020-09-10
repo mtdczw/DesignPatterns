87b3e9db3ae43a3040ad559f73e2eeea8c7e5e49
mtdczw <71051115+mtdczw@users.noreply.github.com>
2020-09-10 15:44:13 +0800

    Add files via upload

---
+++ Memento.java
@@ -0,0 +1,28 @@
+// Memento Design Pattern
+// Used stores an internal state of Originator object at a point of time
+// Memento protects against any other access instead by Originator objects which can
+// store current state and restore to its previous state (undo mechanism)
+
+
+// Memento: The basic object that is stored in different states.
+public class Memento {
+    // The article stored state in Memento Object
+    private String articleItem;
+
+    // 2nd Constructor: To create and save a new article String to a new memento Object
+    public Memento(String articleToSave) {
+        setArticleState(articleToSave);
+    }
+
+    // Return the value stored in article
+    public String getSavedState() {
+        System.out.println("From Memento: Previous Version of Article\n" + " " + articleItem);
+        return articleItem;
+    }
+
+    // Sets the value for article input with new incoming source
+    public void setArticleState(String newArticle) {
+        this.articleItem = newArticle;
+        System.out.println("From Memento: Current Version of Article\n" + " " + articleItem);
+    }
+}
